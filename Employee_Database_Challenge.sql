069b47e01894c87dda951635f193f4516362a0ec
Christopher <christopher@Christophers-MacBook-Pro.local>
2021-02-23 22:15:44 -0500

    Challenge code

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -12,12 +12,19 @@ ON e.emp_no = ti.emp_no
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Export table to CSV
+COPY retirement_titles TO 'C:\\retirement_titles.csv' DELIMITER ',' CSV HEADER;
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Export table to CSV
+COPY unique_titles TO 'C:\\unique_titles.csv' DELIMITER ',' CSV HEADER;
+
