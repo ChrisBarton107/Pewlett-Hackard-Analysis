22c038a5e7bd899ff4e452396525a08a2c61b68f
Christopher <christopher@Christophers-MacBook-Pro.local>
2021-02-24 12:08:44 -0500

    Challenge code

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -41,3 +41,23 @@ COPY retiring_titles TO 'C:\\retiring_titles.csv' DELIMITER ',' CSV HEADER;
 
 
 
+-- Deliverable 2
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+		e.first_name, 
+		e.last_name, 
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON e.emp_no = de.emp_no
+INNER JOIN titles AS ti
+	ON e.emp_no = ti.emp_no
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
+
+-- Export table to CSV
+COPY mentorship_eligibility TO 'C:\\mentorship_eligibility.csv' DELIMITER ',' CSV HEADER;
